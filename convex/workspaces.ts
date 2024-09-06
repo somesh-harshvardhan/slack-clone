a3c073e0dbca6727fc0f008afe0f98125c617098
somesh-harshvardhan <somesh.harshvardhan@gmail.com>
2024-09-06 18:38:14 +0530

    workspaces

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,6 +2,16 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
 
+const generateCode = () => {
+  const code = Array.from(
+    {
+      length: 6,
+    },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+  return code;
+};
+
 export const create = mutation({
   args: {
     name: v.string(),
@@ -11,12 +21,19 @@ export const create = mutation({
     if (!userId) {
       throw new Error("Unauthorized");
     }
-    const joinCode = "1234";
+    const joinCode = generateCode();
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode,
     });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
+
     return workspaceId;
   },
 });
@@ -24,7 +41,25 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await auth.getUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+    return workspaces;
   },
 });
 export const getById = query({
@@ -34,7 +69,13 @@ export const getById = query({
     if (!userId) {
       throw new Error("Unauthorized");
     }
-
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+    if (!members) return null;
     return await ctx.db.get(args.id);
   },
 });
--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -3,15 +3,19 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Loader, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const WorkspaceSwitcher = () => {
+  const router = useRouter();
   const workspaceId = useWorkspaceId();
   const [_open, setOpen] = useCreateWorkspaceModal();
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
@@ -25,16 +29,46 @@ const WorkspaceSwitcher = () => {
   );
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
         <Button className=" size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl ">
-          A
+          {workspaceLoading ? (
+            <Loader className=" size-5 animate-spin shrink-0" />
+          ) : (
+            workspace?.name.charAt(0).toUpperCase()
+          )}
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent
-        side="bottom"
-        align="start"
-        className=" w-64"
-      ></DropdownMenuContent>
+      <DropdownMenuContent side="bottom" align="start" className=" w-64">
+        <DropdownMenuItem
+          onClick={() => router.push(`/workspace/${workspaceId}`)}
+          className=" cursor-pointer flex-col justify-start items-start capitalize"
+        >
+          {workspace?.name}
+          <span className=" text-xs to-muted-foreground">Active workspace</span>
+        </DropdownMenuItem>
+        {filteredWorkspaces?.map((workspace) => (
+          <DropdownMenuItem
+            key={workspace._id}
+            className=" cursor-pointer capitalize"
+            onClick={() => router.push(`/workspace/${workspace._id}`)}
+          >
+            <div className=" size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-lg rounded-md flex items-center justify-center mr-2 ">
+              {" "}
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+            {workspace.name}
+          </DropdownMenuItem>
+        ))}
+        <DropdownMenuItem
+          className=" cursor-pointer"
+          onClick={() => setOpen(true)}
+        >
+          <div className=" size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-lg rounded-md flex items-center justify-center mr-2 ">
+            <Plus />
+          </div>
+          Create a new workspace
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
